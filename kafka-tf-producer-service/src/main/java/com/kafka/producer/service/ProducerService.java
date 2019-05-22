06f2c6fbf176ef987fd0072b60f209b338fc5d91
jeyasekar <50139516+jeyasekar@users.noreply.github.com>
2019-05-22 20:16:54 +0530

    sending pojo 2 kafka

--- kafka-tf-producer-service/src/main/java/com/kafka/producer/service/ProducerService.java
+++ kafka-tf-producer-service/src/main/java/com/kafka/producer/service/ProducerService.java
@@ -1,68 +1,51 @@
 package com.kafka.producer.service;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Service
 public class ProducerService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ProducerService.class);
 
-	private final KafkaTemplate<Integer, String> kafkaTemplate;
+	private final KafkaTemplate<String, Weather> kafkaTemplate;
 
 	@Autowired
-	public ProducerService(final KafkaTemplate<Integer, String> kafkaTemplate) {
+	public ProducerService(KafkaTemplate<String,Weather> kafkaTemplate) {
 		this.kafkaTemplate = kafkaTemplate;
 	}
 
 	public void sendMessage() {
 		String topic = "meenatopic";
+		
+		/*
+		 * File file = new File("C:/KafkaProd/"); File[] files = file.listFiles(); for
+		 * (File f : files) { System.out.println(f.getName()); String fileName =
+		 * f.getName(); FileInputStream fis; try {
+		 * 
+		 * fis = new FileInputStream("C:/KafkaProd/" + fileName); byte[] filevalue = new
+		 * byte[(int) file.length()]; fis.read(filevalue); fis.close();
+		 * 
+		 * String filecontent = new String(filevalue, "UTF-8");
+		 * 
+		 * System.out.println(filecontent);
+		 */
+		for (Weather weather : CountryEnum.getCountryList()) {
 
-		File file = new File("C:/KafkaProd/");
-		File[] files = file.listFiles();
-		for (File f : files) {
-			System.out.println(f.getName());
-			String fileName = f.getName();
-			FileInputStream fis;
 			try {
+				Thread.sleep(2000);
+				System.out.println("sending data='{}' to topic='{}'" + weather.toString() + "" + topic);
+				Message<Weather> message = MessageBuilder.withPayload(weather).setHeader(KafkaHeaders.TOPIC, topic)
+						.build();
+				this.kafkaTemplate.send(message);
 
-				fis = new FileInputStream("C:/KafkaProd/" + fileName);
-				byte[] filevalue = new byte[(int) file.length()];
-				fis.read(filevalue);
-				fis.close();
-
-				String filecontent = new String(filevalue, "UTF-8");
-
-				System.out.println(filecontent);
-
-				{
-					System.out.println("message : " + filecontent);
-					kafkaTemplate.send(topic, filecontent)
-							.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
-
-								@Override
-								public void onSuccess(final SendResult<Integer, String> result) {
-									LOGGER.info("sent message='{}' with offset={}", filecontent,
-											result.getRecordMetadata().offset());
-								}
-
-								@Override
-								public void onFailure(final Throwable exc) {
-									LOGGER.error("unable to send message='{}'", filecontent, exc);
-								}
-							});
-				}
-
-			} catch (IOException e) {
+			} catch (Exception e) {
 
 				e.printStackTrace();
 
